cdf79e691fc013bc84a31784eb056a7158f17d25
T. Hebert <33993883+l4v4d0g@users.noreply.github.com>
2021-06-18 12:59:13 -0400

    Start of Week1 Project 2
    Interactive text-based fiction short story

---
+++ Week1/interactive-fiction/interactive-fiction/interactive-fiction.cpp
@@ -0,0 +1,16 @@
+/***************************************|
+|   Interactive Fiction Project         |
+|   Timothy Hebert                      |
+|   CSC215 - C++ Programming I          |
+|   University of Advancing Technology  |
+|***************************************/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
